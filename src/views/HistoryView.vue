ec8f804e6a1d846b1cae2e593d89e9f6e597158b
Natalia Prokofieva <natalia.prokofieva@mera.com>
2022-04-03 19:06:59 +0300

    chart added

--- src/views/HistoryView.vue
+++ src/views/HistoryView.vue
@@ -5,7 +5,10 @@
     </div>
 
     <div class="history-chart">
-      <canvas></canvas>
+      <Pie
+        :chart-options="chartOptions"
+        :chart-data="chartData"
+      />
     </div>
     <Loader v-if="loading" />
     <p class="center" v-else-if="!records.length">
@@ -36,6 +39,9 @@
 <script>
 import paginationMixin from "@/mixins/pagination.mixin";
 import HistoryTable from "@/components/app/HistoryTable.vue";
+import { Pie } from "vue-chartjs";
+import { Chart, registerables } from "chart.js";
+Chart.register(...registerables);
 
 export default {
   // eslint-disable-next-line vue/multi-word-component-names
@@ -43,29 +49,77 @@ export default {
   mixins: [paginationMixin], //вью мержит поля миксина с полями этой страницы
   data: () => ({
     loading: true,
-    records: []
+    records: [],
+    categories: [],
+    chartOptions: {
+      responsive: true,
+    },
   }),
   async mounted() {
     this.records = await this.$store.dispatch("fetchRecords");
-    const categories = await this.$store.dispatch("fetchCategories");
-    
-    //передаем массив записей в миксин как allItems
-    this.setupPagination(
-      (this.records = this.records.map((record, idx) => {
-        return {
-          ...record,
-          categoryName: categories.find((c) => c.id === record.categoryId)
-            .name,
-          typeClass: record.type === "cost" ? "red" : "green",
-          typeText: record.type === "cost" ? "Расход" : "Доход",
-          number: idx
-        };
-      }))
-    );
+    this.categories = await this.$store.dispatch("fetchCategories");
+    this.setup();
+
     this.loading = false;
+  },
+  methods: {
+    setup() {
+      //передаем массив записей в миксин как allItems
+      this.setupPagination(
+        (this.records = this.records.map((record, idx) => {
+          return {
+            ...record,
+            categoryName: this.categories.find(
+              (c) => c.id === record.categoryId
+            ).name,
+            typeClass: record.type === "cost" ? "red" : "green",
+            typeText: record.type === "cost" ? "Расход" : "Доход",
+            number: idx,
+          };
+        }))
+      );
+    },
   },
   components: {
     HistoryTable,
+    Pie,
+  },
+  computed: {
+    chartData() {
+      return {
+        labels: this.categories.map((c) => c.name),
+        datasets: [
+          {
+            label: "Расходы по категориям",
+            data: this.categories.map((c) => {
+              return this.records.reduce((total, r) => {
+                if (r.categoryId === c.id && r.type === "cost") {
+                  total += +r.amount;
+                }
+                return total;
+              }, 0);
+            }),
+            backgroundColor: [
+              "rgba(255, 99, 132, 0.2)",
+              "rgba(54, 162, 235, 0.2)",
+              "rgba(255, 206, 86, 0.2)",
+              "rgba(75, 192, 192, 0.2)",
+              "rgba(153, 102, 255, 0.2)",
+              "rgba(255, 159, 64, 0.2)",
+            ],
+            borderColor: [
+              "rgba(255, 99, 132, 1)",
+              "rgba(54, 162, 235, 1)",
+              "rgba(255, 206, 86, 1)",
+              "rgba(75, 192, 192, 1)",
+              "rgba(153, 102, 255, 1)",
+              "rgba(255, 159, 64, 1)",
+            ],
+            borderWidth: 1,
+          },
+        ],
+      };
+    },
   },
 };
 </script>
